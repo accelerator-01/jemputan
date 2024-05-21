4c7d5b2cd8f1c4a31e12518e5ff8fe4eedf52e1c
accelerator-01 <acceleratorgame01@gmail.com>
2024-05-21 22:47:31 +0700

    Create welcome screen (beta)

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,5 +1,21 @@
+import welcome_img from "../assets/welcome_img.png";
+
 function Welcome() {
-  return <div>welcome screen</div>;
+  return (
+    <div className="bg-blue-500 h-screen flex flex-col justify-center items-center">
+      <img
+        src={welcome_img}
+        alt="Welcome"
+        className="w-3/4 md:w-1/2 lg:w-1/3 mb-4"
+      />
+      <h1 className="text-white text-3xl md:text-4xl lg:text-5xl font-bold mb-2">
+        Halo~
+      </h1>
+      <p className="text-white text-lg md:text-xl lg:text-2xl">
+        Mari kita jaga ketertiban jalan ^_^
+      </p>
+    </div>
+  );
 }
 
 export default Welcome;
