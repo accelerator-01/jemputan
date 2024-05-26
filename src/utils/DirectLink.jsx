e57f1aa47daef2fb2003c3881a9b4cd41eca1770
accelerator-01 <acceleratorgame01@gmail.com>
2024-05-26 21:29:41 +0700

    ternyata lupa push komponen errornya wkwkwk

--- src/utils/DirectLink.jsx
+++ src/utils/DirectLink.jsx
@@ -1,8 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 function DirectLink({ children, to, onClick }) {
+  const navigate = useNavigate();
   const className = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
+
+  if (to === "-1")
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
 
   if (onClick) {
     return (
